273f5a1f3025e298c94ac1d84a301523661baa04
SantiNones <santiagonones@gmail.com>
2025-07-23 17:18:17 +0000

    Generador Final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+function domainCreator () {
+    let pronoun = ["el","un","nuestro","vuestro"];
+    let adj = ["lindo","guapo","bello","precioso"];
+    let noun = ["momento","regalo","sujeto","abrazo"];
+    let sub = [".net",".si",".no",".woo"];
 
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) { 
+      for (let l = 0; l < sub.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + sub[l]); 
+        }
+      }
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+domainCreator (); 
